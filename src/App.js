465f2b919dd88eae3e8a172cacfbe289022b43fb
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2019-10-03 10:57:46 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import SimpleMDE from 'simplemde';
+import 'simplemde/dist/simplemde.min.css'
+import file from './test-files/pages/privacy.md'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  yamlParser (markdownFile) {
+    // format file to extract yaml front matter
+    const contents = markdownFile.split('---')
+    const articleConfig = contents[1]
+    const articleContent = contents[2]
+
+    // parse yaml and retrieve the attributes like layout etc.
+    const resultsArr = articleConfig.split('\n').map(curr => {
+      if (curr !== '') {
+        return {
+          [curr.split(': ')[0]]: curr.split(': ')[1]
+        }
+      } else {
+        return null
+      }
+    }).filter(function (el) {
+      return el != null;
+    })
+
+    // get the configs all in one object
+    var configObj = {}
+    for (var i = 0; i < resultsArr.length; i++) {
+      configObj = Object.assign(configObj, resultsArr[i])
+    }
+
+    return {
+      configObj,
+      content: articleContent
+    }
+  }
+  
+  async componentDidMount() {
+    // load editor
+    const editor = new SimpleMDE()
+    editor.toggleSideBySide()
+   
+    // load editor with file 
+    const result = await fetch(file)
+    const markdown = await result.text()
+  
+    // parse the content
+    const markdownOutput = this.yamlParser(markdown)
+
+    // test
+    console.log(markdownOutput.configObj)
+
+    // load the editor with the original text
+    editor.value(markdownOutput.content)
+
+    // 
+    editor.codemirror.on('change', () => {
+      console.log('a')
+    })
+
+    // editor.value(marked(result))
+    /*
+    fetch(testFilePath)
+    .then(response => {
+      console.log(response.text())
+      return response.text()
+    })
+    .then(text => {
+      this.setState({
+        markdown: marked(text)
+      })
+    })
+    */
+    
+  }
+
+  render() {
+
+    return (
+      <textarea rows="4" cols="50"></textarea>
+    )
+  }
 }
 
 export default App;
